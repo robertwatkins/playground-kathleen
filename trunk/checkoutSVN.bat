5e2e6b29cae3e0185d33e3cb64d61fe282abbe0c
robert@watkins.net <robert@watkins.net@cc27d782-ffdd-d011-19be-039d421e561a>
2014-12-24 22:54:58 +0000

    spellcheck...

--- trunk/checkoutSVN.bat
+++ trunk/checkoutSVN.bat
@@ -1,4 +1,4 @@
-REM The first time, create a directory in your user direcory (use 'mkdir %HOMEPATH%\Products ') then run this to get the latest version (may overwrite your own changes)
+REM The first time, create a directory in your user direcory (use 'mkdir %HOMEPATH%\Projects ') then run this to get the latest version (may overwrite your own changes)
 
 cd %HOMEPATH%\Projects
 svn checkout https://playground-kathleen.googlecode.com/svn/trunk/ playground-kathleen --username robert@watkins.net
