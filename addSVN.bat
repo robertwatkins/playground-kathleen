21dbdde27aad32e43466217deab9f6704dab87ec
robert@watkins.net <robert@watkins.net@cc27d782-ffdd-d011-19be-039d421e561a>
2014-12-25 20:49:14 +0000

    log message

---
+++ addSVN.bat
@@ -0,0 +1,6 @@
+REM You will need to get your SVN password from https://code.google.com/hosting/settings
+REM You will also need to use your google password after the --username setting below
+
+cd %HOMEPATH%/Projects/playground-kathleen
+svn add *  --force --username robert@watkins.net
+PAUSE
