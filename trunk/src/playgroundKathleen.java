7a3e5ac0a1b76de3771925ded539f05d9aa181cf
robert@watkins.net <robert@watkins.net@cc27d782-ffdd-d011-19be-039d421e561a>
2015-01-01 00:02:09 +0000

    log message

---
+++ trunk/src/playgroundKathleen.java
@@ -0,0 +1,17 @@
+/**
+ * @(#)playground-kathleen.java
+ *
+ * playground-kathleen application
+ *
+ * @author 
+ * @version 1.00 2014/12/25
+ */
+ 
+public class playgroundKathleen {
+    
+    public static void main(String[] args) {
+    	
+    	// TODO, add your application code
+    	System.out.println("Hello World!");
+    }
+}
