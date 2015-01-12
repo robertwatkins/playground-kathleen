44997f1bf3d8226946ee2a57e107158f428af00d
robert@watkins.net <robert@watkins.net@cc27d782-ffdd-d011-19be-039d421e561a>
2015-01-12 02:52:02 +0000

    removing some old files

--- addSVN.bat
+++
@@ -1,6 +0,0 @@
-REM You will need to get your SVN password from https://code.google.com/hosting/settings
-REM You will also need to use your google password after the --username setting below
-
-cd %HOMEPATH%/Projects/playground-kathleen
-svn add *  --force --username robert@watkins.net
-PAUSE
