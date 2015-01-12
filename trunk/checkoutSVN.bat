5c4f3d3a9ee06a99c84f35b4183b4c0d920794e5
robert@watkins.net <robert@watkins.net@cc27d782-ffdd-d011-19be-039d421e561a>
2015-01-12 02:52:02 +0000

    removing some old files

--- trunk/checkoutSVN.bat
+++
@@ -1,5 +0,0 @@
-REM The first time, create a directory in your user direcory (use 'mkdir %HOMEPATH%\Projects ') then run this to get the latest version (may overwrite your own changes)
-
-cd %HOMEPATH%\Projects
-svn checkout https://playground-kathleen.googlecode.com/svn/trunk/ playground-kathleen --username robert@watkins.net
-PAUSE
--- trunk/commitSVN.bat
+++
@@ -1,6 +0,0 @@
-REM You will need to get your SVN password from https://code.google.com/hosting/settings
-REM You will also need to use your google password after the --username setting below
-
-cd ~/Projects/playground-kathleen
-svn commit -m "log message" --username robert@watkins.net
-PAUSE
